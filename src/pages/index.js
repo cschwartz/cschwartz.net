e05f686bed56c47349aea1621d78cc5314a3bb0e
Christian Schwartz <christian.schwartz@gmail.com>
2020-08-06 18:47:40 +0200

    more text changes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to the new site.</p>
-    <p>Now go build something great.</p>
+    <p>Now go build something even greater great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
